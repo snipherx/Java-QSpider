a9b570b085994f758edec72c95033b5e234b00d7
Ashutosh Singh <ashutosh.singh1998.as@gmail.com>
2026-08-09 11:12:14 +0530

    get and set method in encapsulation

---
+++ OOPs learn/SetterMethod.java
@@ -0,0 +1,8 @@
+public class SetterMethod {
+    public static void main(String[] args) {
+        Stud s1=new Stud();
+        System.out.println(s1.getName());
+        s1.setName("Ashu");
+        System.out.println(s1.getName());
+    }
+}
---
+++ OOPs learn/Stud.java
@@ -0,0 +1,12 @@
+public class Stud {
+    private String name="Ashutosh";
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    
+}
