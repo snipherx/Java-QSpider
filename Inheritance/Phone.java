cbc662e242e442ee980da53d0dc3ad12417d3b38
Ashutosh Singh <ashutosh.singh1998.as@gmail.com>
2026-08-09 11:36:43 +0530

    Inheritance extend keyword

---
+++ Inheritance/Phone.java
@@ -0,0 +1,11 @@
+//package Inheritance;
+
+public class Phone {
+    static void start()
+    {
+        System.out.println("Phone is starting");
+    }
+    public static void main(String[] args) {
+        start();
+    }
+}
---
+++ Inheritance/SmartPhone.java
@@ -0,0 +1,8 @@
+//package Inheritance;
+
+public class SmartPhone extends Phone {
+    public static void main(String[] args) {
+        start();
+    }
+    
+}
